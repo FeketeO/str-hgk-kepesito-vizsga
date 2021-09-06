956a530c1b70d7bd210fae4cfeac22fe1e3410cc
FeketeO <69634719+FeketeO@users.noreply.github.com>
2021-09-06 15:05:52 +0200

    correction on 4

--- backend/src/models/classroom.model.js
+++ backend/src/models/classroom.model.js
@@ -4,5 +4,9 @@ module.exports = mongoose.model('Classroom', mongoose.Schema({
     name: {
         type: String,
         required: true
-    }
+    },
+    buildings: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Building'
+    }]
 }), 'classrooms');
